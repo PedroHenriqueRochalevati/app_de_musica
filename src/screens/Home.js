d022e2e2e5992ccd21962a3e26c66598b56a1658
PedroHenriqueRochalevati <pepe.rolev@gmail.com>
2024-03-28 11:25:02 -0300

    att

---
+++ src/screens/Home.js
@@ -0,0 +1,36 @@
+import React, {useState} from "react";
+import { View, Text, StyleSheet, StatusBar } from "react-native";
+import MusicItem from "../components/MusicItem";
+import { useSafeAreaFrame } from "react-native-safe-area-context";
+
+export default function Home({navigation}) {
+  const [currentPlaying, setCurrentPlaying] = useState(null);
+  const item = {
+    id: '1',
+    title: "Highway to hell",
+    group: "AC/DC",
+    album_image: "https://m.media-amazon.com/images/I/711Hf7rJhFL._UF1000,1000_QL80_.jpg",
+  }
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle="light-content" backgroundColor="#121212"/>
+      <Text style={styles.title}>Minhas músicas</Text>
+      <MusicItem isPlaying={() => currentPlaying == item.id} music={item} navigation={navigation} onPlayPause={() => {}}/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: "#121212",
+    paddingTop: 16,
+  },
+  title: {
+    fontSize: 36,
+    fontWeight: "bold",
+    color: "#fff",
+    marginBottom: 20,
+    marginLeft: 20,
+  },
+})
