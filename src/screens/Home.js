b081355fae301d0b1b26658aea151d6771719d33
PedroHenriqueRochalevati <pepe.rolev@gmail.com>
2024-04-04 12:03:09 -0300

    att

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -10,6 +10,9 @@ export default function Home({navigation}) {
     title: "Highway to hell",
     group: "AC/DC",
     album_image: "https://m.media-amazon.com/images/I/711Hf7rJhFL._UF1000,1000_QL80_.jpg",
+    album: "Higway to Hell",
+    year: 1970,
+    genre: "Heavy Metal",
   }
   return (
     <View style={styles.container}>
---
+++ src/screens/MusicDetails.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+export default function MusicDetails(){
+    return(
+        <View>
+            <Text>
+                Detalhes da música
+            </Text>
+        </View>
+    )
+}
